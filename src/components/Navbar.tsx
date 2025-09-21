e85dd894dfd67130b2d29aafe50d12e2a6ab487c
rams <ramelopanisjr.06@gmail.com>
2025-09-21 15:02:33 +0800

    fix topbarlink issue

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 import React from "react";
 import { topBarLinkTypes } from "./Types";
 
-const topbarLink: topBarLinkTypes = [
+const topbarLink: topBarLinkTypes[] = [
   {
     name: "NEW ARRIVAL",
     link: "#",
